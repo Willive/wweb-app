d590ffa50c0878e3edcc2a98661ddc216c7cafbd
William Wherry <williamwherryiv@gmail.com>
2018-03-30 16:16:05 -0400

    working locally

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import AppComponent from './components/App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import reducer from './reducer'
+import saga from './saga'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+const App = () => (
+  <MuiThemeProvider>
+    <AppComponent />
+  </MuiThemeProvider>
+)
+sagaMiddleware.run(saga)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
